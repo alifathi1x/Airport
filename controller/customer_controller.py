81c84b398f0b37487edad58b5c2e13ffb8bc4711
Ali <ali.abolfathee@yahoo.com>
2024-07-17 01:47:52 +0330

    update

---
+++ controller/customer_controller.py
@@ -0,0 +1,72 @@
+from model.entity.customer_information import CustomerInformation
+from model.service.customer_service import CustomerService
+from model.tools.logger import Logger
+
+class CustomerController:
+    @staticmethod
+    def save(name, family, birth_date, national_id, passport_number):
+        try:
+            customer = CustomerInformation("ali","fathi")
+            CustomerService.save(customer)
+            Logger.info(f"customer Saved - {customer}")
+            return True, customer
+        except Exception as e:
+            Logger.error(f"{e}")
+            return False, f"{e}"
+
+    @staticmethod
+    def edit(id,name, family, birth_date, national_id, passport_number):
+        try:
+            customer = CustomerInformation("ali","fathi")
+            customer.id = id
+            CustomerService.edit(customer)
+            Logger.info(f"customer Edited - {customer}")
+            return True, customer
+        except Exception as e:
+            Logger.error(f"{e}")
+            return False, f"{e}"
+
+    @staticmethod
+    def remove(id):
+        try:
+            customer = CustomerService.remove(id)
+            Logger.info(f"customer ID Removed - {customer}")
+            return True, customer
+        except Exception as e:
+            Logger.error(f"{e}")
+            return False, f"{e}"
+
+
+    @staticmethod
+    def findAll():
+        try:
+            customer_list = CustomerService.find_all()
+            Logger.info(f"customer findAll()")
+            return True, customer_list
+        except Exception as e:
+            Logger.error(f"{e}")
+            return False, f"{e}"
+
+
+    @staticmethod
+    def find_by_name(name):
+        try:
+            customer = CustomerService.find_by_name(name)
+            Logger.info(f"customer find by name - {customer}")
+            return True, customer
+        except Exception as e:
+            Logger.error(f"{e}")
+            return False, f"{e}"
+
+    @staticmethod
+    def find_by_family(family):
+        try:
+            customer = CustomerService.find_by_family(family)
+            Logger.info(f"customer find by family - {customer}")
+            return True, customer
+        except Exception as e:
+            Logger.error(f"{e}")
+            return False, f"{e}"
+
+
+
