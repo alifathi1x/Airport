23e951357934b6201c8e2a9dd0ccd5bb1145e142
Messbah <Messbah.A@Gmail.com>
2024-07-22 18:53:30 +0330

    debuged

--- test/airport_ticket_test.py
+++ test/airport_ticket_test.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 my_date = datetime.now()
 
-customer = CustomerInformation("ali","abolfathi",my_date,"999999999","9999999")
+customer = CustomerInformation("ali","abolfathi",my_date,"999999991","9999991")
 customer_da = DataAccess(CustomerInformation)
 customer_da.save(customer)
 print(customer)
